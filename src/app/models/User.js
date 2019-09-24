85e918b1fb247c3db0a16f22d7d9f9d731f69c0f
AnnyCaroline <annycarolinegnr@gmail.com>
2019-09-24 12:21:03 -0300

    Model de usuário

---
+++ src/app/models/User.js
@@ -0,0 +1,24 @@
+import { Model, Sequelize } from 'sequelize';
+
+class User extends Model {
+    // metodo chamado automaticamente pelo sequelize
+    static init(sequelize) {
+        super.init(
+            {
+                // colunas que vamos ter na nossa tabela
+                // podemos evitar columas de chave
+                // primária, chave estrangeira, e até as updated_at e created_at
+
+                // Em outras palavras, só as colunas inseridas pelo usuário
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password_hash: Sequelize.STRING,
+                provider: Sequelize.BOOLEAN,
+            },
+            {
+                sequelize,
+                // veja mais opções com CTRL+SPACE
+            }
+        );
+    }
+}
